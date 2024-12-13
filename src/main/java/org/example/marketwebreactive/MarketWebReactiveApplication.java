9c1726aa64f6499cc1dfba827411ec2550432a03
Zaraza-it <egor76522@Inbox.ri>
2024-12-14 00:04:52 +0300

    Initial commit

---
+++ src/main/java/org/example/marketwebreactive/MarketWebReactiveApplication.java
@@ -0,0 +1,13 @@
+package org.example.marketwebreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketWebReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketWebReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/marketwebreactive/MarketWebReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.marketwebreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketWebReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
